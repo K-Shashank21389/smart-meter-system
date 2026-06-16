ca1c56515e8f421aada7d85cb5230446856c4120
K.Shashank <shashankkunduru49@gmail.com>
2026-06-16 17:38:06 +0530

    Switched email service to Brevo

--- email_service.py
+++ email_service.py
@@ -1,51 +1,17 @@
 from fastapi_mail import FastMail, MessageSchema, ConnectionConfig
-from dotenv import load_dotenv
 import os
-
-load_dotenv()
 
 conf = ConnectionConfig(
     MAIL_USERNAME=os.getenv("MAIL_USERNAME"),
     MAIL_PASSWORD=os.getenv("MAIL_PASSWORD"),
-    MAIL_FROM=os.getenv("MAIL_USERNAME"),
+    MAIL_FROM=os.getenv("MAIL_FROM"),
 
-    MAIL_SERVER="smtp.gmail.com",
-    MAIL_PORT=465,
+    MAIL_SERVER="smtp-relay.brevo.com",
+    MAIL_PORT=587,
 
-    MAIL_SSL_TLS=True,
-    MAIL_STARTTLS=False,
+    MAIL_STARTTLS=True,
+    MAIL_SSL_TLS=False,
 
     USE_CREDENTIALS=True,
     VALIDATE_CERTS=True
 )
-
-async def send_bill_email(
-        email,
-        meter_no,
-        amount,
-        pdf_file
-):
-
-    message = MessageSchema(
-        subject="Electricity Bill",
-        recipients=[email],
-        body=f"""
-Dear Consumer,
-
-Your electricity bill has been generated.
-
-Meter Number: {meter_no}
-Amount: ₹{amount}
-
-Please find attached bill PDF.
-""",
-        subtype="plain",
-        attachments=[pdf_file]
-    )
-
-    print("MAIL_USERNAME =", os.getenv("MAIL_USERNAME"))
-    print("MAIL_PASSWORD EXISTS =", bool(os.getenv("MAIL_PASSWORD")))
-
-    fm = FastMail(conf)
-
-    await fm.send_message(message)
